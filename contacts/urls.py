0bbef6ca618bf77af17f3740c8480488b59670e5
sherkajon <sherkajon@gmail.com>
2020-07-01 09:10:50 +0400

    Changes

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.mainpage),
-    path('testpage', views.testpage)
+    path('', views.mainpage, name="home"),
+    path('testpage', views.testpage),
+    path('createcontact', views.createContact, name="createContact"),
+    path('updatecontact/<int:id>', views.updateContact, name="updateContact")
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Contact
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import ContactForm
 
 
 def mainpage(request):
@@ -12,5 +13,32 @@ def testpage(request):
     return HttpResponse('This is a test page!')
 
 
+def createContact(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        #print(request.POST)
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/contacts')
 
 
+    context = {'form':form}
+    return render(request, 'contacts_form.html', context)
+
+
+def updateContact(request, id):
+
+    contact = Contact.objects.get(id=id)
+    form = ContactForm(instance=contact)
+
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('/contacts')
+
+
+    context = {'form': form}
+    return render(request, 'contacts_form.html', context)
